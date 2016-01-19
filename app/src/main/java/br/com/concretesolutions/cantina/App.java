848ca352b5ea715f1ff61257983b16dca5394b3f
davi rodrigues <davi.silva@concretesolutions.com.br>
2016-01-19 19:33:30 -0200

    configurando push do parse

--- app/src/main/java/br/com/concretesolutions/cantina/App.java
+++ app/src/main/java/br/com/concretesolutions/cantina/App.java
@@ -3,6 +3,7 @@ package br.com.concretesolutions.cantina;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 
 import br.com.concretesolutions.cantina.data.type.parse.Credentials;
@@ -17,8 +18,10 @@ public class App extends Application {
         ParseObject.registerSubclass(Product.class);
         ParseObject.registerSubclass(Credentials.class);
         ParseObject.registerSubclass(Sale.class);
+
         Parse.initialize(this, "z5kcJ98mrUkJ6V3ZGvNIYGiCizld9w9Se65I6zIO",
                 "FeyojuuN6VhgR2hk1Qa7k5e3mZ9klyuOrXLWSWh5");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 
 }
